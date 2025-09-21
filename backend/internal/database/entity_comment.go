ed1815d74187f1ecbd1806bc27029b8e9694211e
Mohamed AlAlawi <mohwalalawi@gmail.com>
2025-09-21 21:14:28 +0300

    Add new properties to the Comment struct and adapt the sql queries to the new database schema

--- backend/internal/database/entity_comment.go
+++ backend/internal/database/entity_comment.go
@@ -7,20 +7,21 @@ import (
 
 type Comment struct {
 	ID        int       `db:"id" json:"id"`
-	Username  string    `db:"username" json:"username"`
+	UserID    int       `db:"user_id" json:"user_id"`
 	Content   string    `db:"content" json:"content"`
+	Image     []byte    `db:"image" json:"image"`
 	CreatedAt time.Time `db:"created_at" json:"created_at"`
 }
 
-func (db DB) InsertComment(postID, userID int, content string, createdAt string) (int, error) {
+func (db DB) InsertComment(postID int, userID int, content string, image []byte, createdAt string) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
 	defer cancel()
 
 	query := `
-    INSERT INTO comment (post_id, user_id, content, created_at)
-    VALUES ($1, $2, $3, $4)`
+    INSERT INTO comment (post_id, user_id, content, image, created_at)
+    VALUES ($1, $2, $3, $4, $5)`
 
-	result, err := db.ExecContext(ctx, query, postID, userID, content, createdAt)
+	result, err := db.ExecContext(ctx, query, postID, userID, content, image, createdAt)
 	if err != nil {
 		return 0, err
 	}
@@ -33,22 +34,20 @@ func (db DB) InsertComment(postID, userID int, content string, createdAt string)
 	return int(id), err
 }
 
-// GetPaginatedCommentsForPost returns paginated comments for a specific post
-func (db *DB) GetPaginatedCommentsForPost(postID int, offset, limit int) ([]Comment, error) {
+func (db *DB) GetCommentsForPost(postID int) ([]Comment, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
 	defer cancel()
 
 	var comments []Comment
 
 	query := `
-    SELECT c.id, u.username, c.content, c.created_at
+    SELECT c.id, u.id, u.f_name, u.l_name, c.content, c.image, c.created_at
     FROM comment c
     JOIN user u ON c.user_id = u.id
     WHERE c.post_id = $1
-    ORDER BY c.created_at ASC
-    LIMIT $2 OFFSET $3`
+    ORDER BY c.created_at ASC`
 
-	err := db.SelectContext(ctx, &comments, query, postID, limit, offset)
+	err := db.SelectContext(ctx, &comments, query, postID)
 	if err != nil {
 		return nil, err
 	}

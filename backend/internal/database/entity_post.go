3f2025194e0437ed16997609c5d6ef5660b31889
aalasmawi <asmawiabdulla0@gmail.com>
2025-10-09 18:56:49 +0300

    Add GET functions for public, private & limited post to entity_post.go

--- backend/internal/database/entity_post.go
+++ backend/internal/database/entity_post.go
@@ -81,3 +81,142 @@ func (db *DB) InsertPost(userID int, content string, file []byte, currentDateTim
 
 	return int(id), nil
 }
+
+func (db *DB) GetPostByID(postID int) (*Post, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+
+	var post Post
+
+	query := `
+		SELECT p.id, u.f_name, u.l_name, u.avatar, p.content, p.file, p.created_at,
+		COALESCE(COUNT(c.id), 0) as comment_count
+		FROM post p
+		JOIN user u ON p.user_id = u.id
+		LEFT JOIN comment c ON p.id = c.post_id
+		WHERE p.id = $1
+		GROUP BY p.id, u.f_name, u.l_name, u.avatar, p.content, p.file, p.created_at`
+
+	err := db.GetContext(ctx, &post, query, postID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &post, nil
+}
+
+func (db*DB) PrivatePostsByUserID(userID int) ([]Post, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+
+	var posts []Post
+
+	query := `
+		SELECT p.id, u.f_name, u.l_name, u.avatar, p.content, p.file, p.created_at,
+		COALESCE(COUNT(c.id), 0) as comment_count
+		FROM post p
+		JOIN user u ON p.user_id = u.id
+		LEFT JOIN comment c ON p.id = c.post_id
+		WHERE p.user_id = $1 AND p.is_private = TRUE
+		GROUP BY p.id, u.f_name, u.l_name, u.avatar, p.content, p.file, p.created_at
+		ORDER BY p.created_at DESC`
+
+	err := db.SelectContext(ctx, &posts, query, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
+//gets all private & public posts that the user can see
+func (db *DB) AllPosts(userID int) ([]Post, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+
+	var posts []Post
+	
+	query := ` SELECT 
+    p.id,
+    u.f_name,
+    u.l_name,
+    u.avatar,
+    p.content,
+    p.file,
+    p.created_at,
+    COALESCE(COUNT(c.id), 0) AS comment_count
+	FROM post p
+	JOIN user u 
+    ON p.user_id = u.id
+	LEFT JOIN comment c 
+    ON p.id = c.post_id
+	WHERE 
+			(
+				p.visibility = 'public'
+
+				OR (
+					p.visibility = 'private'
+					AND EXISTS (
+						SELECT 1 
+						FROM follow_request f
+						WHERE f.requester_id = $1       -- current user
+						AND f.target_id = p.user_id   -- poster
+						AND f.status = 'accepted'
+					)
+				)
+
+				OR p.user_id = $1
+			)
+	GROUP BY 
+    p.id, u.f_name, u.l_name, u.avatar, p.content, p.file, p.created_at
+	ORDER BY 
+    p.created_at DESC;`
+
+	err := db.SelectContext(ctx, &posts, query, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
+
+// Gets all limited posts visible to the given user
+func (db *DB) LimitedPostsForUser(userID int) ([]Post, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+
+	var posts []Post
+
+	query := `
+		SELECT 
+			p.id,
+			u.f_name,
+			u.l_name,
+			u.avatar,
+			p.content,
+			p.file,
+			p.created_at,
+			COALESCE(COUNT(c.id), 0) AS comment_count
+		FROM post p
+		JOIN user u 
+			ON p.user_id = u.id
+		LEFT JOIN comment c 
+			ON p.id = c.post_id
+		JOIN post_user_can_view pcv 
+			ON pcv.post_id = p.id
+		WHERE 
+			p.visibility = 'limited'
+			AND pcv.user_id = $1  
+		GROUP BY 
+			p.id, u.f_name, u.l_name, u.avatar, p.content, p.file, p.created_at
+		ORDER BY 
+			p.created_at DESC;
+	`
+
+	err := db.SelectContext(ctx, &posts, query, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}

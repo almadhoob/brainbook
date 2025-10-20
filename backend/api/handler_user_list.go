4fd23a50e00cc1638b108700344a47c5ba9e3daa
aalasmawi <asmawiabdulla0@gmail.com>
2025-10-20 09:09:04 +0300

    Adjust user code on the database, remove pagination from user list

--- backend/api/handler_user_list.go
+++ backend/api/handler_user_list.go
@@ -7,13 +7,6 @@ import (
 )
 
 // TO DO: This has to change as the conversation table will do most of the heavy lifting
-type UserListResponse struct {
-	ID              int     `json:"id"`
-	FullName        string  `json:"full_name"`
-	Status          int     `json:"status"` // 0 = offline, 1 = online
-	LastMessageTime *string `json:"last_message_time"`
-}
-
 func (app *Application) getUserList(w http.ResponseWriter, r *http.Request) {
 	user := contextGetAuthenticatedUser(r)
 
@@ -24,56 +17,20 @@ func (app *Application) getUserList(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Convert to response format (status will be updated by frontend from WebSocket cache)
-	var userList []UserListResponse
-	for _, User := range users {
-		userList = append(userList, UserListResponse{
-			ID:              User.ID,
-			FullName:        User.FName + " " + User.LName,
-			Status:          0, // Default to offline, frontend will update from WebSocket cache
-			LastMessageTime: User.LastMessageTime,
+	var usersWithFullName []map[string]any
+
+	for _, user := range users {
+		usersWithFullName = append(usersWithFullName, map[string]any{
+			"user_full_name": user.FullName(),
+			"user_avatar":    user.Avatar,
+			"last_message_time": user.LastMessageTime,
 		})
 	}
 
-	// Get total count excluding current user (consistent with pagination query)
-	totalCount, err := app.DB.TotalUserCountExcludingUser(user.ID)
-	if err != nil {
-		app.serverError(w, r, err)
-		return
-	}
-
-	// Get online count from WebSocket manager (excluding current user)
-	onlineCount := 0
-	offlineCount := totalCount
-	if app.WSManager != nil {
-		onlineUserIDs := app.WSManager.GetOnlineUserIDs()
-
-		// Exclude current user from online count
-		for _, userID := range onlineUserIDs {
-			if userID != user.ID {
-				onlineCount++
-			}
-		}
-
-		offlineCount = totalCount - onlineCount
-
-		// Ensure counts are not negative
-		if onlineCount < 0 {
-			onlineCount = 0
-		}
-		if offlineCount < 0 {
-			offlineCount = 0
-		}
-	}
-
-	// Prepare response with pagination metadata (same pattern as posts)
 	responseData := map[string]interface{}{
-		"users":         userList,
-		"online_count":  onlineCount,
-		"offline_count": offlineCount,
+		"users": usersWithFullName,
 	}
-
-	// Return the paginated user list as JSON
+	
 	err = response.JSON(w, http.StatusOK, responseData)
 	if err != nil {
 		app.serverError(w, r, err)

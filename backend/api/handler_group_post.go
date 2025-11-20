1591f7dda99b6b36973f1410d4794d39c3c72f18
aalasmawi <asmawiabdulla0@gmail.com>
2025-11-20 21:39:02 +0300

    Refactor groupPostCreate to include input validation and error handling for content

--- backend/api/handler_group_post.go
+++ backend/api/handler_group_post.go
@@ -1,15 +1,12 @@
 package api
 
 import (
+	"brainbook-api/internal/request"
 	"brainbook-api/internal/response"
-	"net/http"
 	t "brainbook-api/internal/time"
+	"brainbook-api/internal/validator"
+	"net/http"
 )
-
-var input struct {
-	Content string `json:"content"`
-	File    []byte `json:"file"`
-}
 
 func (app *Application) groupPosts(w http.ResponseWriter, r *http.Request) {
 
@@ -45,6 +42,24 @@ func (app *Application) groupPosts(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Application) groupPostCreate(w http.ResponseWriter, r *http.Request) {
+
+	var input struct {
+		Content   string              `json:"content"`
+		File      []byte              `json:"file"`
+		Validator validator.Validator `json:"-"`
+	}
+
+	if err := request.DecodeJSON(w, r, &input); err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	input.Validator.CheckField(validator.NotBlank(input.Content), "content", "Content must not be empty")
+	input.Validator.CheckField(validator.MaxRunes(input.Content, 500), "content", "Content must not exceed 500 characters")
+	if input.Validator.HasErrors() {
+		app.failedValidation(w, r, input.Validator)
+		return
+	}
 
 	ctx := contextGetAuthenticatedUser(r)
 	userID := ctx.ID
